3a2650989bb12401297d29f1d7a10c3b3d480e48
Manny Alfaro <mannyalfa@gmail.com>
2021-09-17 20:21:36 -0400

    it works!

---
+++ client/build/precache-manifest.7b5094131c22a66d9649ba56f3873f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3713fc3ea2c96e54c466fe5e70fd343c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a49e1e3f2e60dac951",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "b3765f731f905d72f2a6",
+    "url": "/static/js/2.7633bdbc.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7633bdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a49e1e3f2e60dac951",
+    "url": "/static/js/main.7622e194.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
